9e092efb5e1763c3b6d565645bdc7c390cacaa86
ArtemK <kanin@bk.ru>
2022-02-06 00:02:30 +0300

    добавлены тесты создания контактов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String nickName;
+    private final String homePhone;
+    private final String eMail;
+
+    public ContactData(String firstName, String lastName, String nickName, String homePhone, String eMail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.homePhone = homePhone;
+        this.eMail = eMail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+}

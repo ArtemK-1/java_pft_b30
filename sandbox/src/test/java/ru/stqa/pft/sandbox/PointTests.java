de46a6bb77054d748c0cddc6c21e4e370383e1d4
ArtemK <kanin@bk.ru>
2022-02-06 16:31:04 +0300

    PointTests: разбиение на независимые тесты

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,12 +6,24 @@ import org.testng.annotations.Test;
 public class PointTests {
 
     @Test
-    public void TestDistance(){
+    public void TestDistance1(){
         Point p1 = new Point(-1, 0);
         Point p2 = new Point(-1, 10);
         Assert.assertEquals(p1.distance(p2), 10);
-        Assert.assertEquals(p2.distance(p2), 0);
         Assert.assertEquals(p2.distance(p1), 10);
+    }
+
+    @Test
+    public void TestDistance2(){
+        Point p1 = new Point(-1, 0);
+        Point p2 = new Point(-1, 10);
+        Assert.assertEquals(p2.distance(p2), 0);
+    }
+
+    @Test
+    public void TestDistance4(){
+        Point p1 = new Point(-1, 0);
+        Point p2 = new Point(-1, 10);
         Assert.assertEquals(p1.distance(p2), p2.distance(p1));
     }
 }

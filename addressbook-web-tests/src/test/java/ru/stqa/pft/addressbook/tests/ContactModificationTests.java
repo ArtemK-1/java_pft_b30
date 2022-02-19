b5d971c8fdbeacce44972ee87af023f9be0ba33e
ArtemK <kanin@bk.ru>
2022-02-19 13:23:53 +0300

    Созданы классы ContactDeletionTests и ContactModificationTests + вспомогательные методы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase{
+
+    @Test
+    public void testContactModification(){
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().selectContact();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("Ivan edit", "Ivanov edit", "iv", "+79001230001", "mail@mail.ru"));
+        app.getContactHelper().updateContact();
+    }
+}
